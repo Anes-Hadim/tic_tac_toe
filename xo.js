c4ea0d3af17817d4270b2a1e91db1194f8df27fd
Anes Hadim <hadim1962@hotmail.com>
2024-07-12 16:19:06 +0000

    added minimax algorithm
    now it is impossible to win the computer

--- xo.js
+++ xo.js
@@ -24,38 +24,95 @@ async function start() {
   } 
 }
 
+
 function computer() {
-  let line=Math.floor(Math.random()*3);
-  let colomn=Math.floor(Math.random()*3);
-  if (grid[line][colomn]===0&&!result()) {
-    document.getElementById(`${line}${colomn}`).innerHTML=`<img class="game" src="o.png">`;
-    grid[line][colomn]=1;
+  if (!result(grid,false)) {
+    let bestScore = -Infinity
+    let bestMove;
+    for(let i=0;i<3;i++) {
+      for(let j=0;j<3;j++) {
+        if(grid[i][j]===0) {
+          grid[i][j]=1;
+          let score = minimax(grid,0,false);
+          grid[i][j]=0;
+          if(score>bestScore) {
+            bestScore = score;
+            bestMove = [i,j];
+          }
+        }
+      }
+    }
+    document.getElementById(`${bestMove[0]}${bestMove[1]}`).innerHTML=`<img class="game" src="o.png">`;
+    grid[bestMove[0]][bestMove[1]]=1;
     who=true;
-    result();
+    result(grid,false);
+  }
+}
+
+let scores = {
+  computer: 1,
+  human: -1,
+  tie: 0
+}
+
+function minimax(board,depth, ismax) {
+  let score = result(board,true);
+  if (score !=='') {
+    return scores[score]
+  }
+
+  if (ismax) {
+    let bestScore = -Infinity
+    for(let i=0;i<3;i++) {
+      for(let j=0;j<3;j++) {
+        if(board[i][j]===0) {
+          board[i][j]=1;
+          let score = minimax(board,depth+1,false);
+          board[i][j]=0;
+          if(score>bestScore) {
+            bestScore = score;
+          }
+        }
+      }
+    }
+    return bestScore;
   } else {
-    computer()
+    let bestScore = Infinity
+    for(let i=0;i<3;i++) {
+      for(let j=0;j<3;j++) {
+        if(board[i][j]===0) {
+          board[i][j]=2;
+          let score = minimax(board,depth+1,true);
+          board[i][j]=0;
+          if(score<bestScore) {
+            bestScore = score;
+          }
+        }
+      }
+    }
+    return bestScore;
   }
 }
 
 async function xo(line,colomn) {
-  if (grid[line][colomn]===0&&who&&!result()) {
+  if (grid[line][colomn]===0&&who&&!result(grid,false)) {
     document.getElementById(`${line}${colomn}`).innerHTML=`<img class="game" src="x.png">`;
     grid[line][colomn]=2;
     who=false;
-    result()
+    result(grid,false)
     await delay(1000);
     computer()
   }
 }
 
-function result() {
+function result(board,test) {
   let p = document.getElementById('sd');
   const showResult = (message) => {
     p.innerHTML = `<p id="score">${message}</p>
     <button id="retry"><a href="xo.html"><img id="arrow" src="refresh.png"></a></button>`;
   };
 
-  const checkLine = (a, b, c) => grid[a[0]][a[1]] === grid[b[0]][b[1]] && grid[b[0]][b[1]] === grid[c[0]][c[1]] && grid[a[0]][a[1]] !== 0;
+  const checkLine = (a, b, c) => board[a[0]][a[1]] === board[b[0]][b[1]] && board[b[0]][b[1]] === board[c[0]][c[1]] && board[a[0]][a[1]] !== 0;
 
   const lines = [
     [[0, 0], [0, 1], [0, 2]], // row 1
@@ -70,18 +127,18 @@ function result() {
 
   for (const line of lines) {
     if (checkLine(...line)) {
-      if(grid[line[0][0]][line[0][1]] === 1) {
-        showResult('you lost');
+      if(board[line[0][0]][line[0][1]] === 1) {
+        !test && showResult('you lost');
         return 'computer';
       }else {
-        showResult('you won');
+        !test && showResult('you won');
         return 'human';
       }
     }
   }
 
-  if (grid.every(row => row.every(cell => cell !== 0))) {
-    showResult("It's a tie!");
+  if (board.every(row => row.every(cell => cell !== 0))) {
+    !test && showResult("It's a tie!");
     return 'tie';
   }
 
